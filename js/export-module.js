f306e705c96b1e5bde74456a6ab93897a3462b8c
denemmy <denemmy@gmail.com>
2018-07-14 11:42:55 +0300

    add export module

---
+++ js/export-module.js
@@ -0,0 +1,20 @@
+export_module = (function() {
+
+	var export_m = {};
+	var inited = false;
+
+	export_m.init = function() {
+		inited = true;
+	}
+
+	export_m.plot = function() {
+
+	}
+
+	export_m.display = function() {
+
+	}
+
+	return export_m;
+
+})();
